ca17641ddee99cd30e39d3abb2d6f505caeb9f92
Martin <omegamastar@gmail.com>
2018-07-18 09:58:05 +0300

    initial view commit

---
+++ View/main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+
+	<title>Blog2rite</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css" />
+
+    <script src="css/js/jquery.min.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="shell">
+                <div class="header__body" id="headerBody">
+
+                </div><!-- /.header__body -->
+			</div><!-- /.shell -->
+		</div><!-- /.header -->
+
+		<div class="main">
+            <div class='container'>
+                <div class='shell'>
+                    <div class="blogs" id="blogs">
+
+                    </div>
+                </div><!-- /.shell -->
+            </div><!-- /.container -->
+		</div><!-- /.main -->
+    </div><!-- /.wrapper -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-echo "yay";
+include "View/main.php";
